ecd0fe53e891d5b5703715558f66540d14ed5d2d
Qichenye <59810094+Qichenye@users.noreply.github.com>
2020-07-27 11:14:23 -0500

    tiny visitor change
    Vistors only have show all posts and login routerlink

--- client/src/components/PostComponent.vue
+++ client/src/components/PostComponent.vue
@@ -1,9 +1,9 @@
 <template>
 <div class = "container">
     <router-link to="ShowPosts" class="showallpostslink">Show All Posts</router-link>
-    <router-link to="AddPost" class="addpostlink">Add Post</router-link>
-    <router-link to="MyPosts" class="mypostslink">My Posts</router-link>
-    <router-link to="Orders" class="orderlink">My Orders</router-link>
+    <router-link to="AddPost" class="addpostlink" v-if="username">Add Post</router-link>
+    <router-link to="MyPosts" class="mypostslink" v-if="username">My Posts</router-link>
+    <router-link to="Orders" class="orderlink" v-if="username">My Orders</router-link>
     <router-link to="MyCart" class="mycartlink" v-if="username">My Cart</router-link>
     <router-link to="Logout" class="logoutlink" v-if="username">Logout</router-link>
     <router-link to="Login" class="logoutlink" v-if="!username">Login</router-link>
