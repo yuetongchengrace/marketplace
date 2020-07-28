08f02fd1a5bf0845849a598eb7ce0b209a4f82b6
yuetongchengrace <54546789+yuetongchengrace@users.noreply.github.com>
2020-07-27 22:50:11 -0500

    do not show buy all button if no item is in the cart

--- client/src/components/MyCartComponent.vue
+++ client/src/components/MyCartComponent.vue
@@ -35,7 +35,7 @@
       @click="navigate(item.itemid)" v-if="!item.sold">Buy</b-button>
     </div>
   </div>
-  <b-button size="lg" @click="buyall()">Buy All</b-button>
+  <b-button size="lg" id="buyall" @click="buyall()" v-if="cartArr.length">Buy All</b-button>
 </div>
 
 </template>
@@ -63,91 +63,95 @@ export default {
     async buyall() {
       let checkSold = 0;
       let money = 0;
-      this.cartArr.forEach((item) => {
-        if (item.sold === 1) {
-          checkSold = 1;
-        } else {
-          money += item.price;
-        }
-      });
-      if (checkSold === 1) {
-        this.$alert('Please clear out the sold items first');
-      } else if (this.balance < money) {
-        this.$alert('You do not have enough balance');
+      if (this.cartArr.length === 0) {
+        alert('no item in cart!');
       } else {
-        this.$confirm('Are you sure?').then(() => {
-          this.cartArr.forEach((item) => {
-            console.log(item);
-            // Update item to sold
-            const url = 'http://localhost:4000/api/posts/sell/';
-            const id = item.itemid;
-            axios.post(`${url}${id}`).then((res) => {
-              console.log(res.status);
-            }).catch((err) => {
-              console.log(err);
+        this.cartArr.forEach((item) => {
+          if (item.sold === 1) {
+            checkSold = 1;
+          } else {
+            money += item.price;
+          }
+        });
+        if (checkSold === 1) {
+          this.$alert('Please clear out the sold items first');
+        } else if (this.balance < money) {
+          this.$alert('You do not have enough balance');
+        } else {
+          this.$confirm('Are you sure?').then(() => {
+            this.cartArr.forEach((item) => {
+              console.log(item);
+              // Update item to sold
+              const url = 'http://localhost:4000/api/posts/sell/';
+              const id = item.itemid;
+              axios.post(`${url}${id}`).then((res) => {
+                console.log(res.status);
+              }).catch((err) => {
+                console.log(err);
+              });
+              // Update my cart sold (0->1)
+              const carturl2 = 'http://localhost:4000/api/carts/sell/';
+              axios.post(`${carturl2}${id}`, {
+              }).then((res) => {
+                console.log(res.status);
+              }).catch((err) => {
+                console.log(err);
+              });
+              // Delete from my cart
+              /* eslint no-underscore-dangle: ["error", { "allow": ["_id"] }] */
+              const id1 = item._id;
+              const carturl = 'http://localhost:4000/api/carts/delete/';
+              axios.delete(`${carturl}${id1}`, {
+              }).then((res) => {
+                console.log(res.status);
+              }).catch((err) => {
+                console.log(err);
+              });
+              // Add order in order collection
+              axios.post('http://localhost:4000/api/orders/addorder', {
+                username: this.username,
+                title: item.title,
+                price: item.price,
+                seller: item.seller,
+                picture: item.picture,
+                itemid: item.itemid,
+              }).then((res) => {
+                console.log(res.status);
+              }).catch((err) => {
+                console.log(err);
+              });
+              // deduct Balance for buyer
+              axios.post('http://localhost:4000/api/users/deductBalance', {
+                username: this.username,
+                price: item.price,
+              }).then((res) => {
+                console.log(res.status);
+                this.balance = res.data.balance;
+              }).catch((err) => {
+                console.log(err);
+              });
+              // increase Balance for seller
+              axios.post('http://localhost:4000/api/users/increaseBalance', {
+                username: item.seller,
+                price: item.price,
+              }).then((res) => {
+                console.log(res.status);
+              }).catch((err) => {
+                console.log(err);
+              });
             });
-            // Update my cart sold (0->1)
-            const carturl2 = 'http://localhost:4000/api/carts/sell/';
-            axios.post(`${carturl2}${id}`, {
-            }).then((res) => {
-              console.log(res.status);
-            }).catch((err) => {
-              console.log(err);
-            });
-            // Delete from my cart
-            /* eslint no-underscore-dangle: ["error", { "allow": ["_id"] }] */
-            const id1 = item._id;
-            const carturl = 'http://localhost:4000/api/carts/delete/';
-            axios.delete(`${carturl}${id1}`, {
-            }).then((res) => {
-              console.log(res.status);
-            }).catch((err) => {
-              console.log(err);
-            });
-            // Add order in order collection
-            axios.post('http://localhost:4000/api/orders/addorder', {
-              username: this.username,
-              title: item.title,
-              price: item.price,
-              seller: item.seller,
-              picture: item.picture,
-              itemid: item.itemid,
-            }).then((res) => {
-              console.log(res.status);
-            }).catch((err) => {
-              console.log(err);
-            });
-            // deduct Balance for buyer
-            axios.post('http://localhost:4000/api/users/deductBalance', {
-              username: this.username,
-              price: item.price,
-            }).then((res) => {
-              console.log(res.status);
-              this.balance = res.data.balance;
-            }).catch((err) => {
-              console.log(err);
-            });
-            // increase Balance for seller
-            axios.post('http://localhost:4000/api/users/increaseBalance', {
-              username: item.seller,
-              price: item.price,
-            }).then((res) => {
-              console.log(res.status);
-            }).catch((err) => {
-              console.log(err);
-            });
+            this.$alert('Payment Successful!');
+            // const obj2 = { username: this.username };
+            // this.cart = await CartService.getMyCart(obj2);
+            // this.cartArr = await CartService.getMyCart(obj2);
+            window.location.href = 'http://localhost:8080/#/Orders';
+          }).catch((err) => {
+            console.log(err);
           });
-          this.$alert('Payment Successful!');
           // const obj2 = { username: this.username };
           // this.cart = await CartService.getMyCart(obj2);
           // this.cartArr = await CartService.getMyCart(obj2);
-          window.location.href = 'http://localhost:8080/#/Orders';
-        }).catch((err) => {
-          console.log(err);
-        });
-        // const obj2 = { username: this.username };
-        // this.cart = await CartService.getMyCart(obj2);
-        // this.cartArr = await CartService.getMyCart(obj2);
+        }
       }
     },
     navigate(id) {
@@ -250,5 +254,8 @@ a {
   margin-right:20px;
   margin-top:10px;
   font-size:15px;
+}
+#buyall{
+  margin-top:30px;
 }
 </style>
